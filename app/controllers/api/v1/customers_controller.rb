21d4c5040ffff3003e6d5a32e928483491880e3f
Jason Wright <noobjey@gmail.com>
2015-11-03 22:34:23 -0700

    customer uses base controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,23 +1,23 @@
-class Api::V1::CustomersController < ApplicationController
+class Api::V1::CustomersController < Api::V1::BaseController
 
   def index
-    respond_with Customer.all
+    super(Customer)
   end
 
   def show
-    respond_with Customer.find(params[:id])
+    super(Customer)
   end
 
   def find
-    respond_with Customer.insensitive_find_by(customer_params).first
+    super(Customer)
   end
 
   def find_all
-    respond_with Customer.where(customer_params)
+    super(Customer)
   end
 
   def random
-    respond_with Customer.all.sample(1).first
+    super(Customer)
   end
 
   def invoices
@@ -28,18 +28,10 @@ class Api::V1::CustomersController < ApplicationController
     respond_with Transaction.joins(:invoice).where(["customer_id = ?", params[:id]])
   end
 
-  def favorite_merchant
-    # byebug
-    # GET /api/v1/customers/:id/favorite_merchant returns a merchant where the customer has conducted the most successful transactions
-    # merchant who customer has invoices with
-    # was thinking doing a merge but not sure
-    # all invoices with successful transactions
-    #   Invoice.where(customer_id: params[:id]).joins(:transact).where(["result = ?", "success" ])
-  end
-
+  
   private
 
-  def customer_params
+  def allowed_params
     params.permit(:id, :first_name, :last_name)
   end
 end
